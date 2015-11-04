f4d79fae59e9ceb46d36688746bc89fedb262774
Johan <Johan@Johan-PC>
2015-11-04 10:56:53 +0100

    s

---
+++ src/becker/robots/Start.java
@@ -0,0 +1,44 @@
+package becker.robots;
+
+
+
+public class Start extends City {
+
+    int size;
+
+    public Start(int size) {
+        super(size, size);
+
+        this.size = size;
+    }
+
+    public void distributeThings(int numThings) {
+
+        showThingCounts(true);
+
+        for (int i = 0; i < numThings; i++)
+            new Thing(this, randomInt(size), randomInt(size));
+    }
+
+    void putWalls() {
+
+        for (int i = 0; i < size; i++) {
+            new Wall(this, 0, i, Direction.NORTH);
+            new Wall(this, size-1, i, Direction.SOUTH);
+            new Wall(this, i, 0, Direction.WEST);
+            new Wall(this, i, size-1, Direction.EAST);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Start world = new Start(11);
+        world.putWalls();
+        world.distributeThings(20);
+        Robot rob = new Robot(world, 5, 5, Direction.NORTH);
+
+
+    }
+
+}
